7f79893f3eabb99854cbee59efc6e0deea205488
Ryo Ando <sasigume@gmail.com>
2021-05-06 05:23:36 +0900

    changed predeploy command

--- predeploy.sh
+++ predeploy.sh
@@ -4,6 +4,7 @@ filename="crowdin.yml"
 replace="REPLACE_THIS_WITH_API_TOKEN"
 with=$CROWDIN_API_TOKEN
 
+# this won't work on macos
 if [[ $replace != "" && $with != "" ]]; then
-  sed -i .bak "s/$replace/$with/" $filename
+  sed -i "s/$replace/$with/" $filename
 fi
