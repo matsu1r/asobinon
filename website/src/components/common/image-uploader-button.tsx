e13328053c1fc29b2d80c809501d772654c6643c
Ryo Ando <sasigume@gmail.com>
2021-05-20 22:59:38 +0900

    [GUI] サイドバーに画像アップボタンをつけた

--- website/src/components/common/image-uploader-button.tsx
+++ website/src/components/common/image-uploader-button.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 export default function ImageUploaderButton() {
   return (
-    <a href="https://client.asobinon.org/authenticated/images/upload">
+    <a
+      target="_blank"
+      href="https://client.asobinon.org/authenticated/images/upload"
+    >
       <div className="button button--primary">画像アップローダー</div>
     </a>
   );
