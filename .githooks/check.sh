b231a8219d5c318c323e30445c77746de20fe0e3
Ryo Ando <sasigume@gmail.com>
2021-05-23 16:32:37 +0900

    [hooks] 変更防止hooksを実装

---
+++ .githooks/check.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+runOnChange() {
+	echo -e "$1"
+}
+FILES_PATTERN='(eula|CODE_OF_CONDUCT|LICENSE)'
+if git diff --cached --name-only | grep -qE $FILES_PATTERN; then
+    runOnChange "\033*******************************************************************************\n*重要なファイルが変更されました。事前にイシューで相談の上マージしてください。 *\n*******************************************************************************"
+    exit 1;
+else
+    exit 0;
+fi
