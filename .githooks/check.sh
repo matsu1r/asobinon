5e36238634a4a1725d1ca1276261784cd55d536b
Ryo Ando <sasigume@gmail.com>
2021-05-23 16:36:38 +0900

    [hook] 大文字対応

--- .githooks/check.sh
+++ .githooks/check.sh
@@ -2,7 +2,7 @@
 runOnChange() {
 	echo -e "$1"
 }
-FILES_PATTERN='(eula|CODE_OF_CONDUCT|LICENSE)'
+FILES_PATTERN='(eula|EULA|CODE_OF_CONDUCT|LICENSE|license)'
 if git diff --cached --name-only | grep -qE $FILES_PATTERN; then
     runOnChange "\033*******************************************************************************\n*重要なファイルが変更されました。事前にイシューで相談の上マージしてください。 *\n*******************************************************************************"
     exit 1;
