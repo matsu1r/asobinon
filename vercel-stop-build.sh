9627bc31dbf29a032bae39d2bb88047cdfd80a62
さしぐめ <74000913+sasigume@users.noreply.github.com>
2021-06-12 04:14:42 +0900

    Create vercel-stop-build.sh

---
+++ vercel-stop-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# main以外はデプロイキャンセルする(制限が怖いので)
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" ]] ; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
